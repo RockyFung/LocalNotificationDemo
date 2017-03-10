c57b50c79e0165405525cd92b66c82088e189586
rockyMni <fengjian0213@126.com>
2017-03-10 16:26:35 +0800

    Initial Commit

---
+++ LocalNotificationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalNotificationDemo
+//
+//  Created by rocky on 2017/3/10.
+//  Copyright © 2017年 RockyFung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
